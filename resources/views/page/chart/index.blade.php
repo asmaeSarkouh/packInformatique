cd8414ac0a0e107ca6d042bc5934a88360688231
asmaeSarkouh <106113813+asmaeSarkouh@users.noreply.github.com>
2023-05-18 23:35:21 +0200

    Update index.blade.php

--- resources/views/page/chart/index.blade.php
+++ resources/views/page/chart/index.blade.php
@@ -3,136 +3,58 @@
 <head>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
   <script>
-
   document.addEventListener('DOMContentLoaded', function() {
-    // var ctx = document.getElementById('myChart').getContext('2d');
+    var falseCount = {{ $falseCount }};
+            var trueCount = {{ $trueCount }};
 
-    // Fetch the data from the server using AJAX or from a variable
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var data = @json($data);
-    console.log(data);
-    var etat_true = [];
-    var etat_false = [];
-    var labels = [];
-    // var data = {
-    //     labels: [],
-    //     etat_true: [],
-    //     etat_false: []
-    // };
-    data.forEach(item => {
-        if (item.etat === true) {
-          etat_true.push(item.etat);
-          etat_false.push(null);
-        } else {
-          etat_true.push(null);
-            etat_false.push(item.etat);
-        }
-        labels.push(item.created_at);
-    });
+            var data = {
+                labels: ['Etats False', 'Etats True'],
+                datasets: [{
+                    data: [falseCount, trueCount],
+                    backgroundColor: ['#FF6384', '#36A2EB'],
+                    hoverBackgroundColor: ['#FF6384', '#36A2EB']
+                }]
+            };
 
-    // Add your AJAX call or assign the data from a variable
-
-    // Create the chart
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: data.labels,
-            datasets: [
-                {
-                    label: 'Etat True',
-                    data: data.etat_true,
-                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Etat False',
-                    data: data.etat_false,
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
+            // Render the chart using Chart.js
+            new Chart(document.getElementById('piechart'), {
+                type: 'pie',
+                data: data,
+                options: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                    },
                 }
-            ]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                x: {
-                    type: 'time',
-                    time: {
-                        unit: 'day',
-                        displayFormats: {
-                            day: 'MMM D'
-                        }
-                    }
-                },
-                y: {
-                    beginAtZero: true,
-                    stepSize: 1
-                }
-            }
-        }
-    });
+            });
 
 
-    // var ctx = document.getElementById('myChart').getContext('2d');
-    // var data = @json($data);
-    // console.log(data);
-    // var trueCount = [];
-    // var falseCount = [];
-    // var labels = [];
-    // console.log(trueCount);
-    // console.log(falseCount);
-    // console.log(labels);
-    // data.forEach(item => {
-    //     if (item.etat === true) {
-    //         trueCount.push(item.etat);
-    //         falseCount.push(null);
-    //     } else {
-    //         trueCount.push(null);
-    //         falseCount.push(item.etat);
-    //     }
-    //     labels.push(item.created_at);
-    // });
 
-    // var chart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //         labels: labels,
-    //         datasets: [{
-    //             label: 'True',
-    //             data: trueCount,
-    //             fill: false,
-    //             borderColor: 'rgba(75, 192, 192, 1)',
-    //             borderWidth: 1
-    //         }, {
-    //             label: 'False',
-    //             data: falseCount,
-    //             fill: false,
-    //             borderColor: 'rgba(255, 99, 132, 1)',
-    //             borderWidth: 1
-    //         }]
-    //     },
-    //     options: {
-    //         responsive: true,
-    //         scales: {
-    //             x: {
-    //                 type: 'time',
-    //                 time: {
-    //                     unit: 'day',
-    //                     displayFormats: {
-    //                         day: 'MMM D'
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // });
   });
 </script>
-
 </head>
 <body>
+ @yield('body')
+    <div class="grid lg:grid-cols-6 gap-4 p-4">
+        <div class="lg:col-span-2 col-span-1 bg-white flex justify-between w-full bodrder p-4 rounded-lg">
+            <div class="flex flex-col w-full pb-4">
+                <p className='text-2xl font-bold'>{{$trueCount}} </p>
+                <p className='text-gray-600'>Material Active</p>
+            </div>
+            <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+            </p>
+        </div>
+        <div class="lg:col-span-2 col-span-1 bg-white flex justify-between w-full bodrder p-4 rounded-lg">
+            <div class="flex flex-col w-full pb-4">
+                <p className='text-2xl font-bold'>{{$falseCount}} </p>
+                <p className='text-gray-600'>Material Desactive</p>
+            </div>
+            <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+            </p>
+        </div>
+    </div>
+    <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white"></div>
+
     <canvas id="myChart"></canvas>
 
 </body>
